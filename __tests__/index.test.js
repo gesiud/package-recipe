d60f92d5246bc55daee0de96b618db4a06049cca
Grzegorz Dabrowski <3940032+gesiud@users.noreply.github.com>
2026-01-01 17:45:09 +0100

    test: add initial test for greet function in index.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,6 @@ describe("index.js", () => {
   it("funkcja greet zwraca  Witaj świecie", () => {
     const greet = require("../src/index");
 
-    expect(greet()).toBe("Witaj świecie");
+    expect(greet()).toBe("Witaj MARSIE KURWA!");
   });
 });
